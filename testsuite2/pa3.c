3d2691864768fd14e6115fd5355a38785d6df185
AaEll <aaronelliot27@gmail.com>
2017-12-10 01:10:00 -0500

    testing stuff

--- testsuite2/pa3.c
+++ testsuite2/pa3.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Basic constants and macros */
 #define WSIZE       4       /* Word and header/footer size (bytes) */
@@ -60,20 +61,22 @@ void Init (size_t size) {
    Head = malloc(sizeof(struct node));
    init_node_types_2(Head,baseptr, baseptr);
    TOTALSIZE = size;
+	printf("value of a = 0x%08x",baseptr);
 }
 
 addrs_t Malloc (size_t size) {
   struct node * pointer;
   struct node * look_ahead;
-  size = size+(0x8-size%0x8)%0x8;
+  size = (size+(0x8-size%0x8)%0x8)/8;
   if(Head == NULL){ // if Init has not been used, then allocate just enough space for this item
-	addrs_t baseptr; // maybe remove this
-	baseptr = (addrs_t) malloc (size)
+	printf("UnitializedError : Init has not been ran	");
+	addrs_t baseptr; // maybe remove this sub-section
+	baseptr = (addrs_t) malloc (size);
     Head = malloc(sizeof(struct node));
 	struct node * new;
 	new = malloc(sizeof(struct node));
 	init_node_types_3(Head,baseptr-1, baseptr,new);
-	init_node_type_2(new,baseptr,baseptr+size+1);	
+	init_node_types_2(new,baseptr,baseptr+size+1);	
 	TOTALSIZE = size;
     return new->start;
   }
@@ -88,11 +91,13 @@ addrs_t Malloc (size_t size) {
         pointer->next = new;
         return new->start;
       }
+	  printf("DXC");
     }
     else if (TOTALSIZE+Head->end - pointer->end >=size){ // IF we reach the end of the linked list, THEN check if there is space
       struct node * new = malloc(sizeof(struct node));
       init_node_types_2(new,pointer->end,pointer->end+size+1);
       pointer->next = new;
+	  printf("valAssigneda = 0x%08x",new->start);
       return new->start;
     }
     pointer = look_ahead;
@@ -115,7 +120,6 @@ void Free (addrs_t addr) {
   }
 }
 
-
 addrs_t Put (any_t data, size_t size) {
   /*TODO add the "data" to the position rtnval */
   addrs_t rtnVal = Malloc (size); 
@@ -126,69 +130,16 @@ addrs_t Put (any_t data, size_t size) {
 
 void Get (any_t return_data, addrs_t addr, size_t size) {
   memmove(return_data, addr, size); //This is giving a segfault
+  /*
+  int i;
+  struct node* pointer = Head;
+  for (i=0; i<size; i++){
+	  if(pointer.start==addr){
+		  *((char*)(return_data)) = *((char*)(addr));
+	  }
+	  pointer = pointer->next;
+  }*/
   Free(addr);
 }
 
-// Part 2
 
-// VMalloc, VFree, VPut, and VGet prototypes initialization
-// Note: These functions behave almost identically to Malloc() and Free(), except they work with POINTERS to addrs_t
-
-addrs_t *VMalloc (size_t size){
-  struct node * pointer;
-  struct node * look_ahead;
-  if(Head == NULL){
-    struct node * new = malloc(sizeof(struct node));
-    init_node_types_3(new,pointer->end,pointer->end+size,pointer->next);
-    pointer->next = new;
-    return new->start;
-  }
-
-  else{
-    pointer = Head->next;
-    size = size+(0x8-size%0x8)%0x8;
-    while(pointer!=NULL){
-      look_ahead = pointer->next;
-      if (look_ahead != NULL){
-        if (look_ahead->start-pointer->end>=size){
-          // Make a new node and set pointer.next = to it, and its pointer to look_ahead
-          struct node * new = malloc(sizeof(struct node));
-          init_node_types_3(new,pointer->end,pointer->end+size,pointer->next);
-          pointer->next = new;
-          return new->start;
-        }
-  }
-}
-
-void VFree (addrs_t *addr){
-  struct node* current = Head;
-  while (current!=NULL){
-    if ( (*(char*)(current->next)) == (*(char*)(addr)) ){
-      current->next = current->next->next;
-      free(addr);
-      break;
-    }
-    current = current->next;
-  }
-}
-
-addrs_t *VPut (any_t data, size_t size) {
-
-   Allocate size bytes from M2 using VMalloc().
-   Copy size bytes of data into Malloc'd memory.
-   You can assume data is a storage area outside M2.
-   Return pointer to redirection table for Malloc'd memory.
-}
-
-void VGet (any_t return_data, addrs_t *addr, size_t size) {
-    Copy size bytes from the memory area, M2, to data address. The
-    addr argument specifies a pointer to a redirection table entry.
-    As with VPut(), you can assume data is a storage area outside M2.
-    Finally, de-allocate size bytes of memory using VFree() with addr
-    pointing to a redirection table entry.
-}
-
-
-int main(){
-  return 0;
-}
