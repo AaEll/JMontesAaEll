2d71d64ed3ce2c3670d8e4ab09d1839c66ec4c93
Justin Montes <montes@builtbyparallel.com>
2017-12-13 13:21:26 -0500

    aaron\'s changes

--- pa32.c
+++ pa32.c
@@ -1,7 +1,3 @@
-// Part 2
-// Authors: Justin Montes Aaron Elliot
-//pa3
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,16 +6,15 @@
 #define WSIZE       4       /* Word and header/footer size (bytes) */
 #define DSIZE       8			/* Doubleword size (bytes) */
 #define CHUNKSIZE  (1<<12)  /* Extend heap by this amount (bytes) */
+#define VHEAP
 
 // Define baseptr and such
 typedef char *addrs_t;
 typedef void *any_t;
 
-// Counting variables for heap Check
-int count_Vmalloc;
-int count_Vfree;
+// Part 1
 
-// Doubly-Linked list architecture for tracking
+// Linked list architecture for tracking
 struct node{
   // start of memory chunk
   addrs_t start;
@@ -27,8 +22,7 @@ struct node{
   addrs_t end;
   // next node in linked list
   struct node *next;
-  // previous item in linked list
-  struct node *prev;
+
 };
 
 // (NC) node null constructor
@@ -52,90 +46,76 @@ void init_node_types_3(struct node* n, addrs_t start, addrs_t end, struct node* 
    n->next = nextNode;
 }
 
-// Init VHead for heap
+// Init head of list
 struct node * Head;
+struct node * Tail;
+size_t TOTALSIZE;
 
-//init array dynamically in heap
-void heap_init(size_t size){
-  addrs_t baseptr;
-  baseptr = (addrs_t) malloc(size);
-  VHead = malloc(sizeof(struct node));
-  init_node_types_2(Head,baseptr, baseptr);
-  TOTALSIZE = size;
-  printf("value of a = %08llx",(uint64_t)baseptr);
+// Init func
+void VInit (size_t size) { // heap alloc
+   addrs_t baseptr;
+   baseptr = (addrs_t) malloc (size);
+   Head = malloc(sizeof(struct node));
+   Tail = Head;
+   init_node_types_2(Head,baseptr, baseptr);
+   TOTALSIZE = size;
 }
+
 
 // VMalloc
-addrs_t *VMalloc (size_t size){
-  count_Vmalloc += 1;
-  struct node * pointer;
-  struct node * look_ahead;
+addrs_t * VMalloc (size_t size) {
   size = (size+(0x8-size%0x8)%0x8);
-  pointer = Head;
-  while(pointer!=NULL){
-    look_ahead = pointer->next;
-    VLook_ahead = look_ahead;
-    if (look_ahead != NULL){
-  		if ((uint64_t)(look_ahead->start)-(uint64_t)(pointer->end)>=size){
-  			// Make a new node and set pointer.next = to it, and its pointer to look_ahead
-  			struct node * new = malloc(sizeof(struct node));
-  			init_node_types_3(new,pointer->end,pointer->end+size,pointer->next);
-  			pointer->next = new;
-        //pointer to pointer
-  			return pointer->start;
-  		}
-    }
-    else if (TOTALSIZE+Head->end - pointer->end >=size){ // IF we reach the end of the linked list, THEN check if there is space
-      struct node * new = malloc(sizeof(struct node));
-      init_node_types_2(new,pointer->end,pointer->end+size);
-      pointer->next = new;
-      //pointer to pointer
-      VPointer->next = pointer;
-      return (addrs_t*)VPointer->start;
-    }
-    pointer = look_ahead->start;
+  if (TOTALSIZE+Head->end - Tail->end >= size){ // IF we reach the end of the linked list, THEN check if there is space
+    struct node * new = malloc(sizeof(struct node));
+    init_node_types_2(new,Tail->end,Tail->end+size);
+    Tail->next = new;
+	Tail = new;
+    return &(new->start);
   }
-  //printf("NoSpaceLeftError : no space left");
-  return (NULL);
+  else{
+    printf("NoSpaceLeftError : no space left\n");
+    return (NULL);
+  }
 }
-
 // VFree
-void VFree (addrs_t *addr){
-  count_Vfree += 1;
-  // frees corresponding node
+void VFree (addrs_t *addr) {
   struct node* current = Head;
-  while (current!=NULL){
-    struct node* temp = current->next;
-    if ( ((char*)(temp->start)) == ((char*)(addr)) ){
-	  current->next = current->next->next;
+  struct node* temp = current->next;
+  while (temp!=NULL){
+	if (((char*)(&(temp->start))) == ((char*)(addr)) ){
+	  current->next = temp->next;
+	  if (temp->next==NULL){
+		Tail = current;
+	  }
 	  free(temp);
-      break;
-    }
+	  break;
+	}
     current = temp;
+	temp = current->next;
   }
-    while(current != NULL){
-      struct node* temp = current->next;
-      if(temp != NULL){
-        memmove(current->end,temp->start,(uint64_t)temp->end-(uint64_t)temp->start);
-        temp->end = current->end+(uint64_t)temp->end-(uint64_t)temp->start;
-        temp->start = current->end;
-      }
-    }
+  temp = current->next;
+  while(temp != NULL){
+	size_t size = temp->end-temp->start;
+	memmove(current->end,temp->start,size);
+	temp -> start = current -> end;
+	temp -> end   = temp->start + size;
+	current = temp;
+	temp = current->next;
+  }
 }
-
 // VPut
-addrs_t *VPut (any_t data, size_t size) {
-     addrs_t* rtnVal = VMalloc (size);
-     if (rtnVal!=NULL){
-   	   memmove( rtnVal,data, size);
-     }
-     return rtnVal;
-   }
+addrs_t* VPut (any_t data, size_t size) {
+  addrs_t* rtnVal = VMalloc (size);
+  if (rtnVal!=NULL){
+    memmove(*rtnVal,data, size);
+  }
+  return rtnVal;
+}
 
 // VGet
 void VGet (any_t return_data, addrs_t *addr, size_t size) {
-    memmove(return_data, addr, size);
-    VFree(addr);
+  memmove(return_data, *addr, size);
+  VFree(addr);
 }
 
 // Heap Checker
